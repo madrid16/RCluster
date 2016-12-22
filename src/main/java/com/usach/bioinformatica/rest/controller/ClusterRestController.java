d2cfcdb087588c6903cdbf12263bcc430fc8001f
Edgar Munoz <edgar.munoz@latam.com>
2016-12-22 16:44:50 -0300

    add url for upload file

--- src/main/java/com/usach/bioinformatica/rest/controller/ClusterRestController.java
+++ src/main/java/com/usach/bioinformatica/rest/controller/ClusterRestController.java
@@ -1,14 +1,19 @@
 package com.usach.bioinformatica.rest.controller;
 
 
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/clustering")
@@ -17,12 +22,24 @@ import javax.ws.rs.core.MediaType;
 
 public class ClusterRestController {
 
+    @Path("/upload")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(
+            @FormDataParam("aFile") InputStream fileInputString,
+            @FormDataParam("aFile") FormDataContentDisposition fileInputDetails) {
+
+        String status = "dasdasdasd";
+        ClusterResponse clusterResponse = new ClusterResponse();
+        clusterResponse.setId(1);
+
+
+        return Response.status(200).entity(clusterResponse).build();
+    }
+
     @RequestMapping(value = "matriz", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     @ApiOperation(value = "Inject a new messagge in Sicco Engine", notes = "...", response = ClusterResponse.class, produces = MediaType.APPLICATION_JSON)
-    @ApiResponses(value = {
-            @ApiResponse(code = 500, message = "code: 500 message: Internal service error </br>", response = String.class),
-            @ApiResponse(code = 200, message = "code: 200 message: Successful </br>", response = ClusterResponse.class)
-    })
     public @ResponseBody ClusterResponse helloworld(@RequestBody String id) {
         ClusterResponse clusterResponse = new ClusterResponse();
         clusterResponse.setId(1);
